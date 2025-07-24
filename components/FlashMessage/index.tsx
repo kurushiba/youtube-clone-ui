07b7e8a77b725653e1fbbc37d49416cf8c488b0c
kurushiba <156669290+kurushiba@users.noreply.github.com>
2025-07-24 20:55:39 +0900

    add components and assets

---
+++ components/FlashMessage/index.tsx
@@ -0,0 +1,21 @@
+import './FlashMessage.css';
+import type { FlashMessage } from '../../modules/flash-message/flash-message.state';
+
+interface Props {
+  message: FlashMessage;
+}
+
+const FlashMessageArea = ({ message }: Props) => {
+  return (
+    <div className="flash-message-container">
+      <div className={`flash-message ${message.type}`}>
+        <span className="flash-message-icon">
+          {message.type === 'error' ? '✕' : '✓'}
+        </span>
+        <span className="flash-message-text">{message.content}</span>
+      </div>
+    </div>
+  );
+};
+
+export default FlashMessageArea;
