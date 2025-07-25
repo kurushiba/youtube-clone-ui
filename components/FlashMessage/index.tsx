d072196ffc0308e0ac4453c56c72e70a5c52acb9
kurushiba <156669290+kurushiba@users.noreply.github.com>
2025-07-25 16:17:53 +0900

    Update index.tsx

--- components/FlashMessage/index.tsx
+++ components/FlashMessage/index.tsx
@@ -1,18 +1,11 @@
 import './FlashMessage.css';
-import type { FlashMessage } from '../../modules/flash-message/flash-message.state';
 
-interface Props {
-  message: FlashMessage;
-}
-
-const FlashMessageArea = ({ message }: Props) => {
+const FlashMessageArea = () => {
   return (
     <div className="flash-message-container">
-      <div className={`flash-message ${message.type}`}>
-        <span className="flash-message-icon">
-          {message.type === 'error' ? '✕' : '✓'}
-        </span>
-        <span className="flash-message-text">{message.content}</span>
+      <div className={`flash-message `}>
+        <span className="flash-message-icon"></span>
+        <span className="flash-message-text"></span>
       </div>
     </div>
   );
